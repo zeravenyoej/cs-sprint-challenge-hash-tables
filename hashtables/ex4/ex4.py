cc0a19bece2bdb949b57a18295a91728dbad9611
Joey Nevarez <josephnevarez12@gmail.com>
2020-07-10 11:14:53 -0700

    finishes ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,20 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    cache = {}
+    for num in a: 
+        new_num = str(num)
+        if new_num.startswith("-"):
+            if num not in cache:
+                cache[num] = 1
+
+    keys = list(cache.keys())
+    new_keys = []
+    for key in keys:
+        new_keys.append(-key)
+
+    for nk in new_keys:
+        if nk in a:
+            result.append(nk)
 
     return result
 
